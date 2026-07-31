2f297655783c286f36ffc1885f43d0f5d1f345d7
hyfiugiii206-hue <hyfiugiii206@gmail.com>
2026-07-31 15:40:54 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'student-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
